be9be1d9692b6f662d52b5ea239b0e4b84b0d389
khussa1n <khussain.qudaibergenov@gmail.com>
2023-08-03 02:55:31 +0600

    modified users entity

--- internal/entity/users.go
+++ internal/entity/users.go
@@ -6,5 +6,4 @@ type Users struct {
 	FirstName string `json:"first_name" db:"first_name" binding:"required"`
 	LastName  string `json:"last_name" db:"last_name" binding:"required"`
 	Password  string `json:"password" db:"hashed_password" binding:"required"`
-	RoleID    int64  `json:"roleID" db:"role_id"`
 }
