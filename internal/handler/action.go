1dcbc822c8d3796a28f9c7b7f9cd195efcd99238
khussa1n <khussain.qudaibergenov@gmail.com>
2023-08-20 20:24:22 +0600

    feat: update

--- internal/handler/action.go
+++ internal/handler/action.go
@@ -15,7 +15,6 @@ func (h *Handler) createAction(ctx *gin.Context) {
 	if err != nil {
 		log.Printf("bind json err: %s \n", err.Error())
 		ctx.JSON(http.StatusBadRequest, &api.Error{
-			Code:    -1,
 			Message: err.Error(),
 		})
 		return
@@ -24,14 +23,12 @@ func (h *Handler) createAction(ctx *gin.Context) {
 	action, err := h.srvs.CreateAction(ctx, &req.Actions)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, &api.Error{
-			Code:    -2,
 			Message: err.Error(),
 		})
 		return
 	}
 
 	ctx.JSON(http.StatusCreated, &api.OK{
-		Code:    0,
 		Message: "success",
 		Data: entity.Roles{
 			ID:       action.ID,
@@ -44,14 +41,12 @@ func (h *Handler) getAllActions(ctx *gin.Context) {
 	action, err := h.srvs.GetAllActions(ctx)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, &api.Error{
-			Code:    -2,
 			Message: err.Error(),
 		})
 		return
 	}
 
 	ctx.JSON(http.StatusCreated, &api.OK{
-		Code:    0,
 		Message: "success",
 		Data:    action,
 	})
@@ -62,7 +57,6 @@ func (h *Handler) deleteAction(ctx *gin.Context) {
 	if err != nil {
 		log.Printf("can not get id: %s \n", err.Error())
 		ctx.JSON(http.StatusBadRequest, &api.Error{
-			Code:    -1,
 			Message: "invalid id param",
 		})
 		return
@@ -71,14 +65,12 @@ func (h *Handler) deleteAction(ctx *gin.Context) {
 	err = h.srvs.DeleteAction(ctx, int64(id))
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, &api.Error{
-			Code:    -2,
 			Message: err.Error(),
 		})
 		return
 	}
 
 	ctx.JSON(http.StatusCreated, &api.OK{
-		Code:    0,
 		Message: "success",
 		Data:    "True",
 	})
